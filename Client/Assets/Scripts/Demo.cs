7761a421cbc674513220ef092a5c098e519e34d3
文岳云 <342002688@qq.com>
2019-04-16 17:44:07 +0800

    DoTween
    DoTween

---
+++ Client/Assets/Scripts/Demo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DG.Tweening;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    /************************* 
+	* 作者： wenyueyun 
+	* 时间： 2019/4/16 17:00:25 
+	* 描述： Demo 
+	*************************/
+    public class Demo
+    {
+        private Text text;
+
+        private void Test()
+        {
+            text.DOText("aa", 1);
+        }
+    }
+}
